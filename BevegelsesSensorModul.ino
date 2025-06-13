8b89381366f5c583c7a3f7428ccc17194514eab1
ipo321 <121672419+ipo321@users.noreply.github.com>
2025-06-13 18:44:07 +0200

    Update BevegelsesSensorModul.ino
    justert kommentarer

--- BevegelsesSensorModul.ino
+++ BevegelsesSensorModul.ino
@@ -1,14 +1,13 @@
 //Kode for bevegelses Sensoren modul.
-//Bevegelses Sensor modulen har 3 essensielle komponenter. 1. HC-SR505 PIR bevegelses sensor. 2. piezo høytaler. 3. NRF2010 reciver/transmitter. 
+//Bevegelses Sensor modulen har 3 essensielle komponenter. 1. HC-SR505 PIR bevegelses sensor. 2. piezo høytaler. 3. NRF24L01 reciver/transmitter. 
 //Når jeg bruker ordet NRF i koden referer jeg til modulen som brukes for å mota signaler. (radio).
-//koden fungerer med at arduinoen står og lytter etter signaler. i perioder hvor ingen signal motas gjør ikke arduinoen noe annet en å sjekke etter signaler. 
-//Når arduinoen mottar ett signal via NRF modulen (radio) vil registrere dette som at det er flamme (flamme = sann).
+//koden fungerer med at arduinoen står å lytter etter signaler. i perioder hvor ingen signal motas gjør ikke arduinoen noe annet en å sjekke etter signaler. 
+//Når arduinoen mottar ett signal via NRF modulen (radio) tolker arduinoen dette som at den andre modulen ser en flamme (flamme = true). 
 //sålenge flamme er sann vil arduinoen ha "aktivert" bevegelses sensoren.
 //Dette gjøres ved å sjekke om bevegelses sensoren registrerer bevegelser.
 //hvis den registrerer bevegelser vil piezo høytaleren slå alarm. 
 //Når arduinoen slutter å mota signal/signaer, vil flamme settes til false og arduinoen slutter å se etter bevegelse.
 //NB alle kommentarene som starter med "*" har som formål å hjelpe koderen når man tester og justerer koden. Disse kan kommenteres ut eller fjernes om ønskelig.  
-
 
 #include <SPI.h> //legger til bibliotek som tillater meg og bruke NRF senderen.
 #include <RF24.h>
@@ -32,8 +31,8 @@ const byte addr[6] = "00001";  // lager adressen til "kanalen" jeg øsnker å mo
 void setup() {
   //setup for bevegelses sensor og piezo. 
   Serial.begin(9600);  
-  pinMode(SENSOR_PIN, INPUT);   //setter sensoren til å ta input, siden jeg bil registrere når det er bevegelse.
-  pinMode(BUZZER_PIN, OUTPUT);   // setter BUZZER_PIN til output. siden jeg ønsker å "sende" lyd ut av piesoen. (output).
+  pinMode(SENSOR_PIN, INPUT);   //setter sensoren til å ta input, siden jeg vil registrere når det er bevegelse.
+  pinMode(BUZZER_PIN, OUTPUT);   // setter BUZZER_PIN til output. siden jeg ønsker å "sende" lyd ut av piezoen. (output).
   noTone(BUZZER_PIN);    //setter piezoen til noTone for at den ikke skal lage lyd. 
 
   bufferMillis = millis();  //millis() gir anntal millisekunder siden arduinoen ble slått på.
@@ -42,7 +41,7 @@ void setup() {
 
 
     //setter opp NRF sender/motaker.
-  radio.begin();   // setter opp radio og tillater meg og kalle på andre radio funskjoner.
+  radio.begin();   // setter opp radio og tillater meg å kalle på andre radio funskjoner.
   radio.openReadingPipe(0, addr);  // setter opp NRF til å motta signal. 0 står for hvilken pipe jeg ønsker å bruke for mottakelse av signal. addr er adressen 
   radio.setPALevel(RF24_PA_MIN);  //senderstyrke settes til det laveste for å spare strøm. 
   radio.startListening();   //NRF senderen settes til å lytte etter signaler. 
@@ -57,7 +56,7 @@ void loop() {
 
   if(flamme == true && millis() > nesteSjekk && bevegelse()){   //denne linjen sjekker om 3 ting er sant/true (statements).
     //sjekken starter med å sjekke om variabelen flamme er true. hvis sann så ->
-    //-> sjekk om millis() er større en nesteSjekk, hvis sann så->
+    //-> sjekk om millis() er større en nesteSjekk (forklaring linje 65), hvis sann så->
     //-> sjekk om bevegelse() er true. metoden bevegelse() returnerer true hvis bevegelses sensoren registrerer bevegelse.
     //hvis alle disse stemmer går vi inn i denne kodeblokken. nå er det flamme, millis() er større en NesteSjekk og noen beveger seg forran sensoren.  
 
@@ -70,13 +69,13 @@ void loop() {
 
  
   if (millis() > bufferMillis && radio.available()) { //denne sjekker om radio.availeble() er true. og at man ikke er i stadiet hvor man nettopp har tømmt køen.
-          //forklarer bufferMillis mekanismen lenger ned i koden. 
+          //forklarer bufferMillis mekanismen lenger ned i koden (linje 87). 
           //radio.available() metoden sjekker om det er noen signlaer som er sendt til NRF motakeren på arduinoen, hvis det er noen vil den returnere true.
           //vi vet at hvis det er sendt et signal til denne aruinoen betyr det at ett lys er tent. 
     flamme = true; //setter variabelen flamme til true.
     Serial.println("FLAMME DETEKTERT! "); //* printer til monitoren, dette brukes for feilsøking og oversikt under kjøring (for koderen).
   }else if(millis() > bufferMillis){
-    flamme = false;} //hvis radio.available() ikke har motatt noen signaler og man ikke venter på bufferMillis setter vi flamme til false.
+    flamme = false;} //hvis radio.available() ikke har motatt noen signaler og man ikke venter på bufferMillis (linje 87 forklaring) setter vi flamme til false.
 
   if(millis() > nesteTom){ //her bruker vi samme prinsipp som med nesteSjekk. 
       radio.flush_rx();//radio.flush_rx() gjør at vi nulstiller køen til radioen. hvis det står mange "signaler" i kø i radioen blir disse nullstilt.
@@ -84,8 +83,9 @@ void loop() {
       //etter denne er kjørt vet vi at hvis radio.available() er sann har det nylig kommet et nytt signal, og flammen er fortsatt tent.
       //hvis denne kjører og radio.available() er false vet vi at vi ikke har fått et nytt signal og lyse vil da være slukket. 
 
-    bufferMillis = millis() + 1050;//formålet med bufferMills er at etter vi har tømmt radioen, gir vi NRF motakeren tid til å motta ett nytt signal om flammen fortsatt er tent
-    nesteTom = millis() + 3000;//setter neste tom til 3 sekunder mere en millis() intervall på å tømme køen er 3 sekunder. 
+    bufferMillis = millis() + 1050;//formålet med bufferMills er at etter vi har tømmt radioen, gir vi NRF motakeren tid til å motta ett nytt signal hvis flammen fortsatt er tent.
+    //1050 gjør at man har nåkk tid siden den andre sender signal med intervall på 1 sekund når lys er tent. 
+    nesteTom = millis() + 3000;//setter nesteTom til 3 sekunder mere en millis() intervall på å tømme køen er 3 sekunder. 
   }
    
 }
@@ -96,8 +96,8 @@ void alarm(){ //metode for at piezoen skal lage alarm lyd.
       for(int i = 0; i < 5; i++){ //dene blokken kjører 5 ganger. denne kan man justre etter hvor mange "beeps" man vil ha.
         tone(BUZZER_PIN, 600);  //tone() er innebygt i arduino . denne metoden sender "bølge" til BUZZER_PIN.
         //tallet 600 er frekvensen (hz) man øsnker at piezoen skal vibrere i. Dette vil da tilsvare hvilke tone man får. høyere tall = lysere tone. 
-        delay(500);  //jeg setter et delay på et havlt sekund. Dette vil gjøre at piezoen holder en konstant tone i et halt sekund.
-        noTone(BUZZER_PIN);  //når delayen er ferdig etter et halt sekund setter jeg noTone(BUZZER_PIN) som gjør at pezoen ikke lager noe lyd.
+        delay(500);  //jeg setter et delay på et halvt sekund. Dette vil gjøre at piezoen holder en konstant tone i et halvt sekund.
+        noTone(BUZZER_PIN);  //når delayen er ferdig etter et halvt sekund setter jeg noTone(BUZZER_PIN) som gjør at piezoen ikke lager noe lyd.
         delay(300);  //jeg venter så i 0,3 sekunder hvor piesoen ikke lager lyd.
         
         //man kan justere delayene ettersom hvor raskt man ønsker pipene skal være og relasjon mellom stille og pip. 
@@ -113,4 +113,3 @@ bool bevegelse(){ //Denne metoden returnerer true dersom bevegelses sensoren reg
   }
   return digitalRead(SENSOR_PIN) == HIGH; //Hvis bevegelses sensoren har registrerer bevegelse vil denne returnere true. 
 }
-
